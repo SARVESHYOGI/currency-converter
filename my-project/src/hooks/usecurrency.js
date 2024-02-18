68cea4b229f43b09137d47f04b51868c3c05b50a
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-02-19 03:10:19 +0530

    ui updated

--- my-project/src/hooks/usecurrency.js
+++ my-project/src/hooks/usecurrency.js
@@ -1,1 +1,17 @@
+import { useState, useEffect } from 'react';
+
+function useCurrency(currency) {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+            .then((response) => response.json())
+            .then((data) => setData(data));
+        console.log(data)
+    }, [currency]);
+    console.log(data)
+    return data;
+}
+
+export default useCurrency;
 // fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
